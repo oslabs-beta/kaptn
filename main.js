a4ca309dcfffbb67b9b0bfa332f1dc94051fda94
Olivia Hodel <ohodel16@gmail.com>
2023-04-10 17:35:33 -0400

    Add vite, electron, react

---
+++ main.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const { app, BrowserWindow } = require('electron');
+
+function createMainWindow() {
+  const mainWindow = new BrowserWindow({
+    title: 'Kaptn',
+    width: 600,
+    height: 600,
+  });
+
+  mainWindow.loadFile(path.join(__dirname, '/index.html'));
+}
+
+app.whenReady().then(() => {
+  createMainWindow();
+});
