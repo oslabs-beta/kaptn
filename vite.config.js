51d5fafb15c404fae5fe72e5cff3e2b5c573ed8a
Olivia Hodel <ohodel16@gmail.com>
2023-04-11 19:19:41 -0400

    remove old server

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-const SERVER = 'http://localhost:3000/';
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
