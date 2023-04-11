f6e7bcc6f102289bc88b3a0f9666ea797087ea5c
Olivia Hodel <ohodel16@gmail.com>
2023-04-11 19:11:40 -0400

    add server functionality

--- main.js
+++ main.js
@@ -8,8 +8,9 @@ function createMainWindow() {
     height: 600,
   });
 
+  // mainWindow.loadFile('/index.html');
   // mainWindow.loadFile(path.join(__dirname, '/index.html'));
-  mainWindow.loadURL('http://localhost:5173/');
+  mainWindow.loadURL('http://localhost:3333/');
 }
 
 app.whenReady().then(() => {
