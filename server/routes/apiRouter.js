3363b5a51c86c2593ff6059d59d5947ba6847cd8
Olivia Hodel <ohodel16@gmail.com>
2023-04-11 14:51:12 -0400

    creates backend server files

---
+++ server/routes/apiRouter.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+const apiController = require('../controllers/apiController.js');
+
+const router = express.Router();
+
+module.exports = router;
---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const apiRouter = require('./routes/apiRouter.js');
+
+const PORT = 3000;
+
+// Body parser
+app.use(express.json());
+
+app.use('/api', apiRouter);
+
+// Endpoint does not exist
+app.use((req, res) => {
+  res.status(404).send('Not Found');
+});
+
+// Global error handler
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+module.exports = app;
