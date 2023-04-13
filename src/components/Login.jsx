d2e1599775d7d51a5d651634eed0d6697154d0d2
Olivia Hodel <ohodel16@gmail.com>
2023-04-13 17:27:07 -0400

    Remove login functionality for ease of dashboard edits

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,51 +4,51 @@ import { Link } from 'react-router-dom';
 // import Cookies from 'js-cookie';
 
 function Login() {
-    const [inputPassword, setInputPassword] = useState("");
-    const [inputUsername, setInputUsername] = useState("");
-    const [loggedIn, setLoggedIn] = useState(false)
-    // const [response, setResponse] = useState("")
-    async function checkLogin(event) {
-      event.preventDefault()
-      try {
-         const response = await fetch('http://localhost:3000/user/login', {
-            mode: 'no-cors',
-            method: "POST",
-            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-            // headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-                username: inputUsername,
-                password: inputPassword
-            })
-        })
-        // const res = await response.json();
-        // console.log(response)
-        // .then((res) => res.json())
-        // .then(res => console.log(res))
-        // .then((res) => {
-        // if (res.data.id) { //alter based on response from backend
-        // console.log("correct input")
-        // window.location.href = "http://localhost:3333/dashboard"; 
-      // } else {
-      //   console.log("incorrect")
-      // }
+  const [inputPassword, setInputPassword] = useState('');
+  const [inputUsername, setInputUsername] = useState('');
+  const [loggedIn, setLoggedIn] = useState(false);
+  // const [response, setResponse] = useState("")
+  async function checkLogin(event) {
+    event.preventDefault();
+    // try {
+    //    const response = await fetch('http://localhost:3000/user/login', {
+    //       mode: 'no-cors',
+    //       method: "POST",
+    //       headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+    //       // headers: {'Content-Type': 'application/json'},
+    //       body: JSON.stringify({
+    //           username: inputUsername,
+    //           password: inputPassword
+    //       })
+    //   })
+    // const res = await response.json();
+    // console.log(response)
+    // .then((res) => res.json())
+    // .then(res => console.log(res))
+    // .then((res) => {
+    // if (res.data.id) { //alter based on response from backend
+    // console.log("correct input")
+    window.location.href = 'http://localhost:4444/dashboard';
+    // } else {
+    //   console.log("incorrect")
+    // }
     // })
-      // .catch(err => console.log(err))
-  // })
-    } catch(err) {
-       console.error("Error: ", err);
-    }
+    // .catch(err => console.log(err))
+    // })
+    // } catch(err) {
+    //    console.error("Error: ", err);
+    // }
   }
   return (
     <div>
-        <h1>Login</h1>
-        <div className = 'loginPage'>
-        <form onSubmit={checkLogin} >
-            <div className ="inputs">
-            <input               
-              type="text"
-              name="username"
-              placeholder = 'username'
+      <h1>Login</h1>
+      <div className='loginPage'>
+        <form onSubmit={checkLogin}>
+          <div className='inputs'>
+            <input
+              type='text'
+              name='username'
+              placeholder='username'
               email={inputUsername}
               onChange={(e) => {
                 setInputUsername(e.target.value);
