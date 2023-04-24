b9c31eac78fa1baf96edb7fd0316b1553791ee49
ncordoves <nataliecordoves@gmail.com>
2023-04-24 14:28:41 -0400

    Converts router to typescript

---
+++ server/routes/apiRouter.ts
@@ -0,0 +1,10 @@
+import * as express from 'express';
+import apiController from '../controllers/apiController.js';
+
+const apiRouter = express.Router();
+
+apiRouter.post('/', apiController.postCommand, (req: express.Request, res: express.Response) => {
+  return res.status(200).json(res.locals.cliResponse);
+});
+
+export default apiRouter;
