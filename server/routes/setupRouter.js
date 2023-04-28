101423f375475f417d2edcd74952fc76a41fc38c
Yining Wang <yiningcw@gmail.com>
2023-04-28 14:07:52 -0400

    Fixes bugs

--- server/routes/setupRouter.js
+++ server/routes/setupRouter.js
@@ -1,17 +1,17 @@
 const express = require('express');
-const { exec, execSync, spawn, spawnSync } =require('child_process');
+const { exec, execSync, spawn, spawnSync } = require('child_process');
 const router = express.Router();
 const setupController = require('../controllers/setupController');
 
 // setupController.promInit
 
-router.get('/promsetup', setupController.promInit,(req, res) => {
-  console.log('in setup router')
+router.get('/promsetup', setupController.promInit, (req, res) => {
+  console.log('in setup router');
   return res.sendStatus(200);
 });
 
 router.get('/grafana', setupController.grafInit, (req, res) => {
-  console.log('Grafana setup complete')
+  console.log('Grafana setup complete');
   return res.sendStatus(200);
 });
 
