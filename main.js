8a9e67a7f5fef13552df6d47ab688c1067eecac8
Brecht Horn <brecht.horn@mac.com>
2023-05-04 07:01:13 -0700

    adds prom and graf buttons

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const { app, BrowserWindow, ipcMain } = require('electron');
-const { exec } = require('child_process');
+// const { exec } = require('child_process');
+const { exec, spawnSync, spawn } = require('child_process');
 
 const isDev = process.env.NODE_ENV === 'development';
 
@@ -44,6 +45,78 @@ ipcMain.on('post_command', (event, arg) => {
     }
     // Handle successful command execution with no errors
     return event.sender.send('post_command', stdout);
+  });
+});
+
+// Listen to prom_setup event
+ipcMain.on('prom_setup', (event, arg) => {
+  // This command adds chart repository to helm
+  spawnSync(
+    'helm repo add prometheus-community https://prometheus-community.github.io/helm-charts',
+    { stdio: 'inherit', shell: true }
+  );
+
+  // Update helm
+  spawnSync('helm repo update', { stdio: 'inherit', shell: true });
+
+  // Install helm chart
+  spawnSync(
+    'helm install prometheus666 prometheus-community/kube-prometheus-stack',
+    { stdio: 'inherit', shell: true }
+  );
+
+  return event.sender.send('prom_setup', 'Prom setup complete');
+});
+
+// Listen to graf_setup event
+ipcMain.on('graf_setup', (event, arg) => {
+  let podName;
+  const getFunc = exec('kubectl get pods', (err, stdout, stderr) => {
+    if (err) {
+      console.log(`exec error: ${err}`);
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+    }
+
+    const output = stdout.split('\n');
+    output.forEach((pod) => {
+      if (pod.includes('prometheus666-grafana')) {
+        [podName] = pod.split(' ');
+      }
+    });
+    console.log('Pod name (Main.js, line 91): ', podName);
+  });
+
+  getFunc.once('close', () => {
+    spawnSync('kubectl apply -f prometheus666-grafana.yaml', {
+      studio: 'inherit',
+      shell: true,
+    });
+    spawnSync(`kubectl delete pod ${podName}`, {
+      stdio: 'inherit',
+      shell: true,
+    });
+    return event.sender.send('graf_setup', 'Grafana setup complete');
+  });
+});
+
+// Listen to forward_ports event
+ipcMain.on('forward_ports', (event, arg) => {
+  const ports = spawn(
+    `kubectl port-forward deployment/prometheus666-grafana 3000`,
+    {
+      shell: true,
+    }
+  );
+
+  ports.stderr.on('data', (data) => {
+    console.log(`grafana port forwarding error: ${data}`);
+  });
+
+  ports.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+    return event.sender.send('prom_setup', 'Port forward complete');
   });
 });
 
